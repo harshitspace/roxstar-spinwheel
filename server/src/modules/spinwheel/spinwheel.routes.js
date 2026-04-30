0af1f82f7f85f480dc9f2000c8734f42b40dbf24
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-04-30 13:26:24 +0530

    feat: add rate limiting middleware on join and create wheel endpoints

--- server/src/modules/spinwheel/spinwheel.routes.js
+++ server/src/modules/spinwheel/spinwheel.routes.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import { createWheel, joinWheel, startWheel, getWheelStatus } from './spinwheel.controller.js';
 import { protect, adminOnly } from '../auth/auth.middleware.js';
 import { validateCreateWheel } from './spinwheel.validator.js';
+import { joinWheelLimiter, createWheelLimiter } from '../../middleware/rateLimiter.js';
 
 const router = Router();
 
-router.post('/',          protect, adminOnly, validateCreateWheel, createWheel);
-router.post('/:id/join',  protect, joinWheel);
+router.post('/',          protect, adminOnly, createWheelLimiter, validateCreateWheel, createWheel);
+router.post('/:id/join',  protect, joinWheelLimiter, joinWheel);
 router.post('/:id/start', protect, adminOnly, startWheel);
 router.get('/:id',        protect, getWheelStatus);
 
